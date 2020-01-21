92d25a99a3bd57416bb57068b033643787cba36c
Brian Norman <b2norman@uwaterloo.ca>
2020-01-20 20:48:18 -0500

    Add basic login

--- app/src/main/java/com/cobresun/behemoth/login/LoginFragment.kt
+++ app/src/main/java/com/cobresun/behemoth/login/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.cobresun.behemoth.login
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +10,20 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.cobresun.behemoth.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.login_fragment.*
+import splitties.toast.toast
 
 class LoginFragment : Fragment() {
 
+    private val RC_SIGN_IN: Int = 8888
     private val navController by lazy { this.findNavController() }
 
     private val viewModel: LoginViewModel by viewModels()
+
+    private val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +35,32 @@ class LoginFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         login_button.setOnClickListener {
-            navController.navigate(R.id.action_loginFragment_to_mainFragment)
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                navController.navigate(R.id.action_loginFragment_to_mainFragment)
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                toast("Didn't sign-in!")
+            }
         }
     }
 
